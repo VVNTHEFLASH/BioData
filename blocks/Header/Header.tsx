992fd39d06467e29c4c5e4045dcc7fed3da04f2d
VVNTHEFLASH <vishnurockhunters@gmail.com>
2023-09-03 22:56:58 +0530

    deployed to gh pages

--- blocks/Header/Header.tsx
+++ blocks/Header/Header.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import HeaderController from './HeaderController'
 
 export default class Header extends HeaderController {
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import './App.css'
 import Header from '../blocks/Header/Header'
 import Main from '../blocks/Main/Main'

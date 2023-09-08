c9dc18e76039b09827342f8095613b469df7d3d2
vishnu vardhan <vishnurockhunters@gmail.com>
2023-09-08 14:35:47 +0530

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import Portfolio from '../blocks/Portfolio/Portfolio'
 import './App.css'
-// import Header from '../blocks/Header/Header'
-// import Main from '../blocks/Main/Main'
 function App() {
 
   return (
     <div className='container'>
-      {/* <Header />
-      <Main /> */}
       <Portfolio />
     </div>
   )
